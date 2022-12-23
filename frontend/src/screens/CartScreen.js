6342e1ee99786d3e2eea2352b81513c7bd0436ca
Mauricio Natacci <mauricionatacci@Mauricios-MacBook-Air.local>
2022-12-23 15:42:52 -0300

    added CartScreen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -11,13 +11,51 @@ const CartScreen = ({ match, location, history }) => {
 
   const dispatch = useDispatch()
 
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
+
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty))
     }
   }, [dispatch, productId, qty])
 
-  return <div>Cart</div>
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <p>
+            Your cart is empty! <Link to='/'>Go Back</Link>
+          </p>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.productId}>
+                <Row>
+                  <Col md={3}>
+                    <Image
+                      src={item.image}
+                      alt={item.name}
+                      fluid
+                      rounded
+                    ></Image>
+                  </Col>
+                  <Col md={2}>
+                    <Link to={`/product/${item.productId}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>$ {item.price}</Col>
+                  <Col md={2}>
+                    <p>Quantity: {item.quantity}</p>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+    </Row>
+  )
 }
 
 export default CartScreen
