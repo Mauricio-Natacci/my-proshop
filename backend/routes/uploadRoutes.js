cfae5b279242cd1d544222449593adfdfe08ebcf
Mauricio Natacci <mauricionatacci@Mauricios-MacBook-Air.local>
2022-12-31 18:01:04 -0300

    fixed bug images s3

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -25,16 +25,19 @@ const storageS3 = multerS3({
   contentType: multerS3.AUTO_CONTENT_TYPE,
   acl: 'public-read',
   key: function (req, file, cb) {
-    console.log('test')
-    console.log('file:', file)
-
     cb(null, uuid() + '-' + file.originalname)
   },
 })
 
 const uploadS3 = multer({ storage: storageS3 })
-router.post('/s3', uploadS3.single('file'), (req, res) => {
-  console.log('test')
-  res.send(req.file.location)
+const middleware = uploadS3.single('file')
+router.post('/s3', (req, res) => {
+  middleware(req, res, function (err) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send(req.file.location)
+    }
+  })
 })
 export default router
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -49,7 +49,7 @@ const ProductEditScreen = ({ match, history }) => {
   const uploadFileHandler = async (e) => {
     const file = e.target.files[0]
     const formData = new FormData()
-    formData.append('image', file)
+    formData.append('file', file)
     setUploading(true)
 
     try {
