e173420df786cfa11acf95f20b8d099fe3ab4eb4
Mauricio <mauricionatacci98@gmail.com>
2023-02-17 15:38:28 -0300

    fixed: import

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './bootstrap.min.css'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store';
+import { App } from './App'
+import { reportWebVitals } from './reportWebVitals'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <Provider store={store}>
-  <App />
-</Provider>
-);
+    <App />
+  </Provider>
+)
 
-reportWebVitals();
+reportWebVitals()
