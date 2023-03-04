f22c4ec5194f34989180e6144dba47e0f486a408
Mauricio <mauricionatacci98@gmail.com>
2023-03-04 01:30:43 -0300

    less env var

--- backend/config/index.ts
+++ backend/config/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 import dotenv from 'dotenv'
 import { NotFoundError } from '../errors/NotFoundError'
 
@@ -10,7 +11,8 @@ export const config = {
   port: process.env.PORT || 5000,
   environment: process.env.NODE_ENV,
   jwtSecret: process.env.JWT_SECRET,
-  playgroundEnabled: process.env.PLAYGROUND_ENABLED === 'true'
+  playgroundEnabled: process.env.PLAYGROUND_ENABLED === 'true',
+  graphqlURI: process.env.GRAPHQL_URI || 'http://localhost:5000/graphql'
 }
 
 if (!config.mongoURI) {
@@ -35,4 +37,12 @@ if (!config.environment) {
 
 if (!config.jwtSecret) {
   throw new NotFoundError('JWT_SECRET environment variable is not set')
+}
+
+if (config.playgroundEnabled) {
+  throw new NotFoundError('PLAYGROUND_ENABLED environment variable is not set')
+}
+
+if (!config.graphqlURI) {
+  throw new NotFoundError('GRAPHQL_URI environment variable is not set')
 }
--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -4,17 +4,10 @@ import { Message } from '../components/Message'
 import { Loader } from '../components/Loader'
 import { useQuery } from '@apollo/client'
 import { GET_ALL_PRODUCTS } from '../graphql/queries/product-query'
+import { ProductType } from '../types/product.type'
 
 export const HomeScreen = () => {
   const { loading, error, data: products } = useQuery(GET_ALL_PRODUCTS)
-
-  type Product = {
-    _id: string
-    name: string
-    image: string
-    description: string
-    price: number
-  }
 
   return (
     <>
@@ -25,7 +18,7 @@ export const HomeScreen = () => {
         <Message variant="danger">{error}</Message>
       ) : (
         <Row>
-          {products.getAllProducts.map((product: Product) => (
+          {products.getAllProducts.map((product: ProductType) => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
               <Product product={product} />
             </Col>
