28f9ebafdbd3928610d07ec22452058ad1596e58
Mauricio <mauricionatacci98@gmail.com>
2023-03-04 00:17:01 -0300

    improvements made

--- frontend/src/graphql/queries/product/product-query.ts
+++ frontend/src/graphql/queries/product/product-query.ts
@@ -1,25 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 import { gql } from '@apollo/client'
 
+export const PRODUCT_FIELDS_FRAGMENT = gql`
+  fragment ProductFields on Product {
+    _id
+    name
+    price
+    description
+    image
+  }
+`
+
 export const GET_ALL_PRODUCTS = gql`
+  ${PRODUCT_FIELDS_FRAGMENT}
   query getAllProducts {
     getAllProducts {
-      _id
-      name
-      price
-      description
-      image
+      ...ProductFields
     }
   }
 `
 
 export const GET_PRODUCT = gql`
+  ${PRODUCT_FIELDS_FRAGMENT}
   query getProduct($id: GetProductInput!) {
     getProduct(input: $id) {
-      _id
-      name
-      price
-      description
-      image
+      ...ProductFields
     }
   }
 `
--- frontend/src/screens/ProductScreen.tsx
+++ frontend/src/screens/ProductScreen.tsx
@@ -17,21 +17,15 @@ type ProductScreenProps = {
 }
 
 export const ProductScreen = ({ history, match }: ProductScreenProps) => {
-  const _id = { _id: match.params.id }
-
-  console.log(_id)
-
-  const {
-    loading,
-    error,
-    data: product
-  } = useQuery(GET_PRODUCT, {
+  const { loading, error, data } = useQuery(GET_PRODUCT, {
     fetchPolicy: 'no-cache',
     variables: { id: { _id: match.params.id } }
   })
 
-  console.log(product)
+  if (loading) return <Loader />
+  if (error) return <Message variant="danger">{error}</Message>
 
+  const product = data.getProduct
   const qty = 1
 
   const addToCartHandler = () => {
@@ -43,42 +37,34 @@ export const ProductScreen = ({ history, match }: ProductScreenProps) => {
       <Link className="btn btn-ligth my-3" to="/">
         Go Back
       </Link>
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant="danger">{error}</Message>
-      ) : (
-        <Row>
-          <Col md={6}>
-            <Image
-              className="rounded"
-              src={product.getProduct.image}
-              alt={product.getProduct.name}
-              fluid
-            ></Image>
-          </Col>
-          <Col md={3}>
-            <ListGroup variant="flush">
-              <ListGroup.Item>
-                <h3>{product.getProduct.name}</h3>
-              </ListGroup.Item>
-              <ListGroup.Item>{product.getProduct.description}</ListGroup.Item>
-              <ListGroup.Item>
-                Price: $ {product.getProduct.price}
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Button
-                  onClick={addToCartHandler}
-                  className="btn-block"
-                  type="button"
-                >
-                  Add To Cart
-                </Button>
-              </ListGroup.Item>
-            </ListGroup>
-          </Col>
-        </Row>
-      )}
+      <Row>
+        <Col md={6}>
+          <Image
+            className="rounded"
+            src={product.image}
+            alt={product.name}
+            fluid
+          ></Image>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>{product.description}</ListGroup.Item>
+            <ListGroup.Item>Price: $ {product.price}</ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                onClick={addToCartHandler}
+                className="btn-block"
+                type="button"
+              >
+                Add To Cart
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
     </>
   )
 }
