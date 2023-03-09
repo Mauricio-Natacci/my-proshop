8a94cc22489d19e6217ea2069b909aab94d29d31
Mauricio <mauricionatacci98@gmail.com>
2023-03-09 00:22:42 -0300

    added user fragment

--- frontend/src/graphql/mutations/user/user.mutation.ts
+++ frontend/src/graphql/mutations/user/user.mutation.ts
@@ -1,12 +1,19 @@
 import { gql } from '@apollo/client'
 
+export const USER_FIELDS_FRAGMENT = gql`
+  fragment UserFields on User {
+    _id
+    name
+    email
+    isAdmin
+  }
+`
+
 export const LOGIN = gql`
+  ${USER_FIELDS_FRAGMENT}
   mutation login($input: LoginInput!) {
     login(input: $input) {
-      _id
-      name
-      email
-      isAdmin
+      ...UserFields
     }
   }
 `
