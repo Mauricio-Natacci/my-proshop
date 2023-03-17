825dde485703e81488d237cd760adbe718362c27
Mauricio <mauricionatacci98@gmail.com>
2023-03-17 11:01:04 -0300

    changed path

--- backend/graphql/index.ts
+++ backend/graphql/index.ts
@@ -50,10 +50,21 @@ async function bootstrap() {
   })
 
   if (config.shouldServeReactApp) {
-    app.use(express.static(path.join(__dirname, 'frontend/build')))
+    app.use(express.static(path.join(__dirname, '../../../../frontend/build')))
 
     app.get('*', (_, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+      res.sendFile(
+        path.resolve(
+          __dirname,
+          '..',
+          '..',
+          '..',
+          '..',
+          'frontend',
+          'build',
+          'index.html',
+        ),
+      )
     })
   } else {
     app.get('/', (_, res) => {
